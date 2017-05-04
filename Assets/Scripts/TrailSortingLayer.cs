4bbc996dbac20a2ac218a0b64ef7f76d223d8bce
Robert Edmonds <robertfedmonds@gmail.com>
2017-05-04 01:09:04 -0400

    Trail works with background
    the bullet trails are now on the same sorting layer as the bullets

---
+++ Assets/Scripts/TrailSortingLayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailSortingLayer : MonoBehaviour {
+
+    public TrailRenderer trail;
+    
+    public string sortingLayerName;
+    public int sortingOrder;
+    
+    // Use this for initialization
+    void Start()
+    {
+        trail.sortingLayerName = sortingLayerName;
+        trail.sortingOrder = sortingOrder;
+    }
+}

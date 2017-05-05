2328f0379c4a9808aeaf04b1e7ee0511ed9973fe
Robert Edmonds <robertfedmonds@gmail.com>
2017-05-04 22:09:15 -0400

    Revert "Making the code more robust"
    This reverts commit 2290f736bbd889635533a825eb8a66280ae68c83.

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class AsteroidController : MonoBehaviour
 {
 
--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Rigidbody2D))]
-
 public class BulletController : MonoBehaviour
 {
     public GameObject bulletTrail;
